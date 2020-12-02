c27e52e4b7526ea7a5b81e9e60ba5b8ad150f02b
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2020-12-02 13:25:32 +0300

    Day 2: valid passwords

---
+++ src/main/scala/Day2Star2.scala
@@ -0,0 +1,16 @@
+import scala.util.matching.Regex
+
+object Day2Star2 extends Main[Int] {
+
+  val File = "day2.txt"
+
+  val regex: Regex = raw"(\d+)-(\d+) (.): (.+)".r
+
+  override def solve(input: List[String]): List[Int] = {
+    val res = input.count {
+      case regex(l, r, char, pass) =>
+        pass(l.toInt - 1) == char.head ^ pass(r.toInt - 1) == char.head
+    }
+    List(res)
+  }
+}

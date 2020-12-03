8602a9d71b85264b5c3e1860be18170106f8c2f4
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2020-12-03 12:27:32 +0300

    Day 3: walking trees

---
+++ src/main/scala/Day3Star1.scala
@@ -0,0 +1,20 @@
+
+object Day3Star1 extends Main[Int] {
+
+  val File = "day3.txt"
+
+  val dx = 3
+  val dy = 1
+
+  val tree = '#'
+
+  override def solve(grid: List[String]): List[Int] = {
+    def rec(x: Int = dx, y: Int = dy): Int = {
+      grid.lift(y).fold(0) { line =>
+        val trees = if (line(x % line.length) == tree) 1 else 0
+        trees + rec(x + dx, y + dy)
+      }
+    }
+    List(rec())
+  }
+}
---
+++ src/main/scala/Day3Star2.scala
@@ -0,0 +1,28 @@
+
+object Day3Star2 extends Main[Int] {
+
+  val File = "day3.txt"
+
+  val deltas = List(
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2),
+  )
+
+  val tree = '#'
+
+  override def solve(grid: List[String]): List[Int] = {
+    val res = deltas.map { case (dx, dy) =>
+      def rec(x: Int = dx, y: Int = dy): Int = {
+        grid.lift(y).fold(0) { line =>
+          val trees = if (line(x % line.length) == tree) 1 else 0
+          trees + rec(x + dx, y + dy)
+        }
+      }
+      rec()
+    }
+    List(res.product)
+  }
+}

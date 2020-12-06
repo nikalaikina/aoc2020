7d1737cd54534b35daa8392f788fb68c67171eba
Vera Nikalaikina <nikolajkina@gmail.com>
2020-12-06 15:33:36 +0300

    Day 6: 26 questions

---
+++ src/main/scala/Day6Star2.scala
@@ -0,0 +1,14 @@
+
+object Day6Star2 extends Main[Int] {
+
+  val File = "day6.txt"
+  val Alphabet = "abcdefghijklmnopqrstuvwxyz".toSet
+
+  override def solve(grid: List[String]): List[Int] = {
+    val questions = grid.foldLeft(List(Alphabet)) {
+      case (acc, "") => Alphabet :: acc
+      case (current :: tail, line) => (line.toSet & current) :: tail
+    }
+    List(questions.map(_.size).sum)
+  }
+}

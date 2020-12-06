d61d9659e1077ff487c14d75a7ff1b9b52ec7052
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2020-12-06 15:33:36 +0300

    Day 6: 26 questions

---
+++ src/main/scala/Day6Star1.scala
@@ -0,0 +1,13 @@
+
+object Day6Star1 extends Main[Int] {
+
+  val File = "day6.txt"
+
+  override def solve(grid: List[String]): List[Int] = {
+    val questions = grid.foldLeft(List(Set.empty[Char])) {
+      case (acc, "") => Set.empty[Char] :: acc
+      case (current :: tail, line) => (line.toSet ++ current) :: tail
+    }
+    List(questions.map(_.size).sum)
+  }
+}

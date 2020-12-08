1d49d23fee6cd2fadbca02efdd300341fe25befe
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2020-12-08 18:01:33 +0300

    Day 8: fix instructions

---
+++ src/main/scala/Day8Star2.scala
@@ -0,0 +1,36 @@
+
+import cats.implicits._
+
+object Day8Star2 extends Main[Int] {
+
+  val File = "day8.txt"
+
+  override def solve(input: List[String]): List[Int] = {
+    val array = input.map { s =>
+      val instr :: x :: Nil = s.split(" ").toList
+      (instr, x.toInt)
+    }.toArray
+
+    def fix(acc: Int = 0, current: Int = 0, visited: Set[Int] = Set.empty, fixed: Boolean = false): Option[Int] = {
+      if (visited(current)) {
+        none[Int]
+      } else if (current == array.length) {
+        acc.some
+      } else {
+        array(current) match {
+          case ("acc", d) =>
+            fix(acc + d, current + 1, visited + current, fixed)
+          case ("nop", _) if fixed =>
+            fix(acc, current + 1, visited + current, fixed)
+          case ("jmp", d) if fixed =>
+            fix(acc, current + d, visited + current, fixed)
+          case (i, d) if !fixed =>
+            fix(acc, current + 1, visited + current, i != "nop") orElse
+              fix(acc, current + d, visited + current, i != "jmp")
+        }
+      }
+    }
+
+    fix().toList
+  }
+}

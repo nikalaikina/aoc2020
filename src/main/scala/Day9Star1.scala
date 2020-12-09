2f195acb283e4d82c1e4ff7425eda6f20ff59d7c
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2020-12-09 10:58:36 +0300

    Day 9: range which sums to the number

---
+++ src/main/scala/Day9Star1.scala
@@ -0,0 +1,21 @@
+
+object Day9Star1 extends Main[Long] {
+
+  val File = "day9.txt"
+
+  val Preamble = 25
+
+  override def solve(input: List[String]): List[Long] = {
+    val a = input.map(_.toLong)
+    val res = a.tails.find { list =>
+      val (preamble, x :: _) = list.splitAt(Preamble)
+      val sums = for {
+        a <- preamble
+        b <- preamble
+        if a != b
+      } yield a + b
+      !sums.contains(x)
+    }
+    res.flatMap(_.drop(Preamble).headOption).toList
+  }
+}
---
+++ src/main/scala/Day9Star2.scala
@@ -0,0 +1,24 @@
+
+object Day9Star2 extends Main[Long] {
+
+  val File = "day9.txt"
+
+  val Number = 15353384L
+
+  override def solve(input: List[String]): List[Long] = {
+    val a = input.map(_.toLong)
+
+    @scala.annotation.tailrec
+    def rec(l: Int = 0, r: Int = 0, sum: Long = a.head): Long = {
+      if (sum == Number) {
+        val range = a.slice(l, r)
+        range.min + range.max
+      } else if (sum < Number) {
+        rec(l, r + 1, sum + a(r + 1))
+      } else {
+        rec(l + 1, r max l + 1, sum - a(l))
+      }
+    }
+    List(rec())
+  }
+}

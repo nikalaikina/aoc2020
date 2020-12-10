eee112b35d62d9480fd531a4dad02dfdf9531aa9
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2020-12-10 12:31:21 +0300

    Day 10: count adapter chains

---
+++ src/main/scala/Day10Star2.scala
@@ -0,0 +1,19 @@
+
+object Day10Star2 extends Main[Long] {
+
+  override val File = "day10.txt"
+
+  override def solve(input: List[String]): List[Long] = {
+    val numbers = input.map(_.toInt)
+    val device = numbers.max + 3
+    val exists = (device :: numbers).toSet
+
+    val res = (1 to device)
+      .foldLeft(List(0L, 0, 1)) { (three, jolts) =>
+        val current = if (exists(jolts)) three.sum else 0
+        three.tail :+ current
+      }
+      .last
+    List(res)
+  }
+}

5c864ce012ffc78069a333a2ca45e51a53059f44
Vera Nikalaikina <nikolajkina@gmail.com>
2020-12-10 12:31:21 +0300

    Day 10: count adapter chains

---
+++ src/main/scala/Day10Star1.scala
@@ -0,0 +1,16 @@
+
+object Day10Star1 extends Main[Int] {
+
+  override val File = "day10.txt"
+
+  val Outlet = 0
+
+  override def solve(input: List[String]): List[Int] = {
+    val sorted = input.map(_.toInt).sorted
+    val chain = Outlet :: (sorted :+ (sorted.last + 3))
+    val zip = chain.init zip chain.tail
+    val ones = zip.count { case (a, b) => b - a == 1 }
+    val threes = zip.count { case (a, b) => b - a == 3 }
+    List(ones * threes)
+  }
+}

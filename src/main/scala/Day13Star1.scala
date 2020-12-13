363530e190358344455cb354d1f689bb9628fe6e
Vera Nikalaikina <nikolajkina@gmail.com>
2020-12-13 22:39:54 +0300

    Day 13.1: bus stop

---
+++ src/main/scala/Day13Star1.scala
@@ -0,0 +1,21 @@
+import scala.math.abs
+import scala.util.Try
+
+object Day13Star1 extends Main[Int] {
+
+  override val File = "inputs/day13.txt"
+
+  override def solve(lines: List[String]): List[Int] = {
+    val fst :: snd :: Nil = lines
+    val current = fst.toInt
+    val buses = snd.split(",").toList.flatMap(b => Try(b.toInt).toOption)
+    val res = buses.map { bus =>
+      val last = (current / bus) * bus
+      bus -> (last + bus - current)
+    }
+    res.foreach(println)
+    val (bus, min) = res.minBy(_._2)
+    List(bus * min)
+  }
+
+}
